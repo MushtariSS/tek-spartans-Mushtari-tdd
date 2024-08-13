217d9c2d0b90bc6743e94798b7c6f933c23c7ef2
XPL <mushti.romin@gmail.com>
2024-08-12 21:34:26 -0400

    first Commitment

---
+++ src/test/java/playground/ActivityOne.java
@@ -0,0 +1,31 @@
+package playground;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ActivityOne {
+    public WebDriver driver;
+    @BeforeMethod
+    public void setUpTest() {
+         driver = new ChromeDriver();
+        driver.get("https://facebook.com");
+    }
+    @Test
+    public void testFacebookTitle(){
+        String applicationTitle = driver.getTitle();
+        Assert.assertEquals(applicationTitle, "xFacebook - log in or sign up");
+    }
+
+    @AfterMethod
+    public void cleanupTest() {
+        driver.quit();
+    }
+    }
+
+
+
