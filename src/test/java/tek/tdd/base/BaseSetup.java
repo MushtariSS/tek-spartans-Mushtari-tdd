f3319ab8fd87b23e99449b1a5611f64d7526bc5f
XPL <mushti.romin@gmail.com>
2024-08-14 01:57:31 -0400

    first Commitment

--- src/test/java/tek/tdd/base/BaseSetup.java
+++ src/test/java/tek/tdd/base/BaseSetup.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
