4d1f3d838b5c01d9d85204762612dce429241b3c
XPL <mushti.romin@gmail.com>
2024-08-14 20:16:55 -0400

    first Commitment

---
+++ src/test/java/tek/tdd/tests/BasicSetupTest.java
@@ -0,0 +1,28 @@
+package tek.tdd.tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tek.tdd.base.UIBaseClass;
+import tek.tdd.pages.HomePage;
+
+public class BasicSetupTest extends UIBaseClass {
+
+    @Test
+    public void validateTopLeftCornerLogo() {
+        String actualLogoText = getElementText(By.className("top-nav__logo"));
+
+        Assert.assertEquals(actualLogoText, "TEKSCHOOL", "Logo text should match");
+    }
+
+    /*
+    Navigate to retail app and make sure top-left corner text in TEKSCHOOL
+    And make sure Sign in button is enabled.
+     */
+    public void validateTopLeftCornerText(){
+        String actualCornerText = getElementText(By.linkText("TEKSCHOOL"));
+        Assert.assertEquals(actualCornerText,"TEKSCHOOL", "Logo text should match");
+        boolean isSignInBtnEnabled = isElementEnabled(HomePage.SING_IN_LINK );
+        Assert.assertTrue(isSignInBtnEnabled);
+    }
+}
---
+++ src/test/java/tek/tdd/utility/SeleniumUtility.java
@@ -0,0 +1,36 @@
+package tek.tdd.utility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import tek.tdd.base.BaseSetup;
+
+import java.time.Duration;
+
+public class SeleniumUtility extends BaseSetup {
+    private WebDriverWait getWait() {
+        return  new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+    }
+    private WebElement waitForVisibility (By locator){
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    public void clickOnElement (By locator){
+        getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+    public void sendKeysToElement (By locator, String value) {
+        WebElement element = waitForVisibility(locator);
+        element.clear();
+        element.sendKeys(value);
+    }
+    public String getElementText (By locator){
+        return waitForVisibility(locator).getText();
+    }
+    public boolean  isElementEnabled(By locator){
+        return waitForVisibility(locator).isEnabled();
+    }
+    public boolean isElementDisplayed(By locator) {
+        return waitForVisibility(locator).isDisplayed();
+    }
+
+
+}
